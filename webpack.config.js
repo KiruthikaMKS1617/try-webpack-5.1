9eb2d61c43ab519c7926c1d003c5d40a2bb7da4d
KiruthikaMKS1617 <mksdevdesk@gmail.com>
2024-05-12 10:16:39 +0530

    publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "./dist"),
+    publicPath: "dist/", //"https://some-cdn.com/", defaultval = "auto"
   },
   mode: "none",
   module: {
