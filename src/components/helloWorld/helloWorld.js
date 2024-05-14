0c38f11062f0506bcd5d3447d04ab0295234b8cd
KiruthikaMKS1617 <mksdevdesk@gmail.com>
2024-05-14 07:09:15 +0530

    babel-loader - handled for class property

--- src/components/helloWorld/helloWorld.js
+++ src/components/helloWorld/helloWorld.js
@@ -1,10 +1,11 @@
 import "./helloWorld.scss";
 class HelloWorld {
+  btnClass = "hello-world-btn";
   render() {
     const body = document.querySelector("body");
     const btn = document.createElement("button");
     btn.innerHTML = "Hello World";
-    btn.classList.add("hello-world-btn");
+    btn.classList.add(this.btnClass);
     body.appendChild(btn);
     btn.onclick = function () {
       console.log("100", 100);
