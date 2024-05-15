fd885a739315f2854206904138b0a1e358125266
KiruthikaMKS1617 <mksdevdesk@gmail.com>
2024-05-15 06:43:06 +0530

    html-webpack-plugin : to create html file in dist folder and updated publicPath as we have index within dist itself, so the js and css files will be directly accessible instead of it being accessible from the dist/

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,14 @@ const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.[contenthash].js",
     path: path.resolve(__dirname, "./dist"),
-    publicPath: "dist/", //"https://some-cdn.com/", defaultval = "auto"
+    publicPath: "", //"https://some-cdn.com/", defaultval = "auto"
   },
   mode: "none",
   module: {
@@ -54,5 +55,6 @@ module.exports = {
       filename: "styles.[contenthash].css",
     }),
     new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin(),
   ],
 };
