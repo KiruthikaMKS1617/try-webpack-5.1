1adc35a7cbd78dc6281c758c338ad70faf7c9340
KiruthikaMKS1617 <mksdevdesk@gmail.com>
2024-05-19 09:39:01 +0530

    - code-splitting : multiple css and js bundle

---
+++ src/kiwi-img.js
@@ -0,0 +1,7 @@
+import KiwiImg from "./components/kiwiImg/kiwiImg";
+import Heading from "./components/heading/heading";
+// import addImage from "./addImage";
+const heading = new Heading();
+heading.render();
+const kiwiImg = new KiwiImg();
+kiwiImg.render();
