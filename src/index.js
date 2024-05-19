d4f8f22ef83562eca8c07b0be50bf911338d86e8
KiruthikaMKS1617 <mksdevdesk@gmail.com>
2024-05-19 07:44:23 +0530

    env var used for mode

--- src/index.js
+++ src/index.js
@@ -6,3 +6,7 @@ heading.render();
 const helloWorldObj = new HelloWorld();
 helloWorldObj.render();
 // addImage();
+if (process.env.NODE_ENV === "production") console.log("PROD");
+else if (process.env.NODE_ENV === "development") console.log("DEV");
+
+helloWorldObj.methodThatDoesntExist();
--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     path: path.resolve(__dirname, "./dist"),
     publicPath: "", //"https://some-cdn.com/", defaultval = "auto"
   },
-  mode: "none",
+  mode: "development",
   module: {
     rules: [
       {
